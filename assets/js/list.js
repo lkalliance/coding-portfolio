32fc21c67416a076f482b52990aa679294420d28
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-04-07 10:02:40 -0500

    Added rendering of sites (oops, wrong branch)

---
+++ assets/js/list.js
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        title: "Be For Reel",
+        shortDesc: "A site where users create and vote on movie-dedicated polls",
+        longDesc: "Be For Reel was a project I worked on as the leader of a five-person team in the University of Minnesota Full Stack Coding Boot Camp. It included a username/password login, poll creation tools tied into IMDb's public API, and polls enriched with IMDb data. Users could submit a comment with their vote. The front page displayed six randomly-chosen polls, with the movie poster of one of its randomly-chosen options. Users each had a profile page, which showed all their created quizzes and all their submitted comments.",
+        siteUrl: "https://be-for-reel.herokuapp.com",
+        repoUrl: "https://github.com/lkalliance/Be-For-Reel",
+        image: false,
+        technologies: [
+            "Node.js", "Express", "MySQL", "Sequelize", "Handlebars", "jQuery", "Bootstrap"
+        ]
+
+    }
+]
